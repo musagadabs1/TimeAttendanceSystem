f0ac312c2a5640782056bace53da293caba6777f
Musa Sule <musagadabs1@.com>
2018-11-16 10:16:42 +0100

    DropDown Not Fixed

---
+++ DropdownTest/Models/FruitUtility.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DropdownTest.Models
+{
+    public class FruitUtility
+    {
+        public int FruitId { get; set; }
+        public string FruitName { get; set; }
+
+    }
+}

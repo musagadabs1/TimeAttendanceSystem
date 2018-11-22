ec958ec50ccef372df59eab55573175a93930c9c
Musa Sule <musagadabs1@.com>
2018-11-22 14:51:56 +0100

    JqueryAdded

---
+++ JqueryTutorial/Controllers/EmployeesController.cs
@@ -0,0 +1,37 @@
+﻿using JqueryTutorial.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JqueryTutorial.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        // GET: Employees
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ViewAll()
+        {
+            return View();
+        }
+        public ActionResult AddOrEdit(int id)
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddOrEdit(Employee employee)
+        {
+            return View(employee);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+    }
+}

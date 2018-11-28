d8f9c463e4d50dab506cad1ba09f49895c45600d
Musa Sule <musagadabs1@.com>
2018-11-28 08:45:29 +0100

    Autocomplete

--- WebApplication2/App_Start/RouteConfig.cs
+++ WebApplication2/App_Start/RouteConfig.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace JqueryTutorial
+namespace WebApplication2
 {
     public class RouteConfig
     {
@@ -16,7 +16,7 @@ namespace JqueryTutorial
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Employee", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
